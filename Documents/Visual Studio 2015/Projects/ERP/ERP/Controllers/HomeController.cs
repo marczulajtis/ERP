0ae541f4f78df921289e2f102aec8d83bff1915e
mulanecka <mulanecka@gmail.com>
2016-11-14 11:31:50 +0100

    first commit

---
+++ Documents/Visual Studio 2015/Projects/ERP/ERP/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using ERP.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            return View("Success", user);
+        }
+    }
+}
